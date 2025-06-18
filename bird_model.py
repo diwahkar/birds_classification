5255c519255b27db5564d7dba1ee82b4db349c2c
Diwakar Dhungana <diwahkar@gmail.com>
2025-06-18 19:00:02 +0545

    apply transfer learning using resnet50

--- bird_model.py
+++ bird_model.py
@@ -10,20 +10,28 @@ class BirdsClassifier(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
 
+        if USE_PRETRAINED:
+            self.model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
+            for param in self.model.parameters():
+                param.requires_grad = False
 
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5, padding=2)
-        self.bn1 = nn.BatchNorm2d(num_features=64)
-        self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, padding=2)
-        self.bn2 = nn.BatchNorm2d(num_features=128)
-        self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=5, padding=2)
-        self.bn3 = nn.BatchNorm2d(num_features=256)
+            num_ftrs =self.model.fc.in_features
+            self.model.fc = nn.Linear(in_features=num_ftrs, out_features=num_classes)
 
-        self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1,1))
-        self._to_linear = 256
+        else:
+            self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=5, padding=2)
+            self.bn1 = nn.BatchNorm2d(num_features=64)
+            self.conv2 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=5, padding=2)
+            self.bn2 = nn.BatchNorm2d(num_features=128)
+            self.conv3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=5, padding=2)
+            self.bn3 = nn.BatchNorm2d(num_features=256)
 
-        self.fc1 = nn.Linear(in_features=self._to_linear, out_features=512)
-        self.dropout1 = nn.Dropout(0.5)
-        self.fc2 = nn.Linear(in_features=512, out_features=num_classes)
+            self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1,1))
+            self._to_linear = 256
+
+            self.fc1 = nn.Linear(in_features=self._to_linear, out_features=512)
+            self.dropout1 = nn.Dropout(0.5)
+            self.fc2 = nn.Linear(in_features=512, out_features=num_classes)
 
 
     def convs(self, x):
